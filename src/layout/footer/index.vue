2ad16a7188bf8df7cb2b5de0494d59eff90cbede
free <2914252014@qq.com>
2024-02-28 19:19:38 +0800

    first commit

---
+++ src/layout/footer/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="footer">
+    后台权限管理系统
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  padding: 20px;
+  display: flex;
+  align-items: center;
+}
+</style>
